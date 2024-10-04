00b9e78337da37b1432076dc50195d04f26fb175
Adarsh <adarshulvekar49@gmail.com>
2024-10-04 19:06:36 +0530

    added /SG_decorators to image path

--- src/components/CustomerStats.js
+++ src/components/CustomerStats.js
@@ -61,14 +61,14 @@ const CustomerStats = () => {
         <div className="flex justify-center items-center space-x-12 mb-12">
             {/* 5+ Million Customers */}
             <div className="flex flex-col items-center">
-                <img src="/images/google-reviews-min.png" alt="5+ Million Icon" className="w-20 h-30 mb-4" />
+                <img src="/SG_decorators/images/medal-min.png" alt="5+ Million Icon" className="w-20 h-30 mb-4" />
                 <h3 className="text-2xl font-bold">5+ Million</h3>
                 <p className="text-gray-500">Happy Customers over 6 years</p>
             </div>
 
             {/* Google Reviews */}
             <div className="flex flex-col items-center">
-                <img src="/images/google-reviews-min.png" alt="Google Reviews" className="w-30 h-12 mb-6" />
+                <img src="/SG_decorators/images/google-reviews-min.png" alt="Google Reviews" className="w-30 h-12 mb-6" />
                 <h3 className="text-2xl font-bold">4.4/5 Rating</h3>
                 <p className="text-gray-500">from 900+ reviews on Google</p>
             </div>
